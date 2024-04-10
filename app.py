d361775d5a1e70a93d0e30fc8b4aa14a251d5489
Mustafa Ansari <mustafaansari@mail.com>
2024-04-10 22:13:39 +0530

    some changes

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect
 from flask_cors import CORS, cross_origin
-import joblib
 import pandas as pd
 import numpy as np
+import modelbit
 
 app = Flask(__name__)
 cors = CORS(app)
-pipeline_model = joblib.load('pipeline_model.joblib')
+
+def get_prediction(name, company, year, kms_driven, fuel_type):
+    result = modelbit.get_inference(
+        region="ap-south-1",
+        workspace="mustafaansari",
+        deployment="predict_car_price",
+        data=[name, company, year, kms_driven, fuel_type]
+    )
+    prediction = result['data']
+    rounded_prediction = round(float(prediction), 2)
+    return str(rounded_prediction)
 
 car = pd.read_csv('Cleaned_Car_data.csv')
 
@@ -29,10 +39,9 @@ def predict():
     fuel_type = request.form.get('fuel_type')
     driven = float(request.form.get('kilo_driven'))  # Convert to float
 
-    prediction = pipeline_model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
-                                                     data=np.array([car_model, company, year, driven, fuel_type]).reshape(1, 5)))
+    prediction = get_prediction(car_model, company, year, driven, fuel_type)
     # Return the prediction as a response
-    return str(np.round(prediction[0], 2))
+    return prediction
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=50002)
--- application.py
+++ application.py
@@ -1,28 +1,11 @@
-from flask import Flask, render_template, request
-from flask_cors import CORS
 import joblib
 import pandas as pd
 import numpy as np
 import gradio as gr
 
-app = Flask(__name__)
-cors = CORS(app)
 pipeline_model = joblib.load('pipeline_model.joblib')
 
 car = pd.read_csv('Cleaned_Car_data.csv')
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    companies = sorted(car['company'].unique())
-    car_models = sorted(car['name'].unique())
-    years = sorted(car['year'].unique(), reverse=True)
-    fuel_types = car['fuel_type'].unique()
-
-    companies.insert(0, 'Select Company')
-    car_models.insert(0, 'Select Model')
-    years.insert(0, 'Select Year')
-    fuel_types.insert(0, 'Select Fuel Type')
-    return render_template('index.html', companies=companies, car_models=car_models, years=years, fuel_types=fuel_types)
 
 def predict(company, car_model, year, fuel_type, driven):
     prediction = pipeline_model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
@@ -45,5 +28,4 @@ iface = gr.Interface(fn=predict,
                      title="Car Price Prediction",
                      description="Enter the details of the car to predict its price.")
 
-if __name__ == '__main__':
-    iface.launch()
+iface.launch()
